429d587b47b3371d593b5ae290fc10b8e65b5d42
vedantmodi2020 <vedantmodi201834@gmail.com>
2022-04-07 15:02:41 +0530

    m

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .task import test_func
 # Create your views here.
 def test(request):
-    test_func.delay()
+    test_func.delay() 
     return HttpResponse("Done")
